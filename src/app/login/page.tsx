9ffd8f8d1429a8f4434112c1592ce8007e8016d6
akankshrv <akankshvijay2109@gmail.com>
2024-03-14 10:29:32 +0530

    signup and login  ui and actions added

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,37 @@
-import React from 'react'
-import Login from './Login'
+import React from "react";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import Login from "./Login";
+import Singup from "../signup/Singup";
 
 const LoginPage = () => {
   return (
-    <div>
-        <Login />
-    </div>
-  )
-}
-
-export default LoginPage
+    <>
+      <div className="flex flex-col items-center pt-20">
+        <Tabs defaultValue="login" className="w-[500px]">
+          <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger value="login">Login</TabsTrigger>
+            <TabsTrigger value="signup">Sign Up</TabsTrigger>
+          </TabsList>
+          <TabsContent value="login">
+            <Card>
+              <CardContent className="">
+                <Login />
+              </CardContent>
+              <CardFooter />
+            </Card>
+          </TabsContent>
+          <TabsContent value="signup">
+            <Card>
+              <CardContent className="">
+                <Singup />
+              </CardContent>
+              <CardFooter />
+            </Card>
+          </TabsContent>
+        </Tabs>
+      </div>
+    </>
+  );
+};
+export default LoginPage;
