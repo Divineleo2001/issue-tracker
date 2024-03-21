2a8f2fd118588e959df9200f8df42f65f49f6555
Divineleo20 <divineleo20@gmail.com>
2024-03-21 12:35:36 +0530

    small change in login

--- src/app/login/Login.tsx
+++ src/app/login/Login.tsx
@@ -14,8 +14,9 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
-import { LoginUser } from "../../actions/login";
+
 import { useRouter } from "next/navigation";
+import { LoginUser } from "../actions/(auth)/login";
 
 
 
